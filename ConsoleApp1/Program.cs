42eb8aa4cc38bb986d86d1236dce50e07f5e857a
Administrator <Administrator@QDMUW8R094TJ8Q1>
2018-12-03 14:50:10 +0800

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -10,6 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("测试");
+            Console.WriteLine("测试");
             Console.WriteLine("测试");
 
         }
